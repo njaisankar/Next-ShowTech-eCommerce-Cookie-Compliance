e8714e81bad61be7ece8c0bb630f3e18c5d85685
njaisankar <42046991+njaisankar@users.noreply.github.com>
2023-06-30 13:19:17 +0530

    Added some more test cases for multiples of 3 and 5

--- SolutionFizzFuzz.Tests.cs
+++ SolutionFizzFuzz.Tests.cs
@@ -6,30 +6,107 @@ using NUnit.Framework;
 public class SolutionTests
 {
     
+    using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+[TestFixture]
+public class SolutionTests
+{
+    //Multiples of 3 and 5 test cases
     [Test]
-    public void Test1()
+    public void MultiplesOfThreeAndFiveTest1()
     {
         var solution = new Solution();
         var expectedResult = solution.PrintFizzBuzz(15);
         Assert.AreEqual("FizzBuzz", expectedResult);
     }
     
+    [Test]
+    public void MultiplesOfThreeAndFiveTest2()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(30);
+        Assert.AreEqual("FizzBuzz", expectedResult);
+    }
+    
+    
+     [Test]
+    public void MultiplesOfThreeAndFiveTest3()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(45);
+        Assert.AreEqual("FizzBuzz", expectedResult);
+    }
+    
+    [Test]
+    public void MultiplesOfThreeAndFiveTest4()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(60);
+        Assert.AreEqual("FizzBuzz", expectedResult);
+    }
+    
     //Test case for Multiples of 3
     [Test]
-    public void MultiplesOfThreeTest()
+    public void MultiplesOfThreeTest1()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(3);
+        Assert.AreEqual("Fizz", expectedResult);
+    }
+    
+    //Test case for Multiples of 3
+    [Test]
+    public void MultiplesOfThreeTest2()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(6);
+        Assert.AreEqual("Fizz", expectedResult);
+    }
+    
+    //Test case for Multiples of 3
+    [Test]
+    public void MultiplesOfThreeTest3()
     {
         var solution = new Solution();
         var expectedResult = solution.PrintFizzBuzz(9);
         Assert.AreEqual("Fizz", expectedResult);
     }
     
+    //Test case for Multiples of 3
+    [Test]
+    public void MultiplesOfThreeTest4()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(36);
+        Assert.AreEqual("Fizz", expectedResult);
+    }
     
     //Test case for Multiples of 5
     [Test]
-    public void MultiplesOfFiveTest()
+    public void MultiplesOfFiveTest1()
     {
         var solution = new Solution();
         var expectedResult = solution.PrintFizzBuzz(25);
+        Assert.AreEqual("Buzz", expectedResult);
+    }
+    
+    
+    
+    [Test]
+    public void MultiplesOfFiveTest3()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(125);
+        Assert.AreEqual("Buzz", expectedResult);
+    }
+    
+    [Test]
+    public void MultiplesOfFiveTest4()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(250);
         Assert.AreEqual("Buzz", expectedResult);
     }
     
@@ -42,9 +119,17 @@ public class SolutionTests
         Assert.AreEqual("22", expectedResult);
     }
     
-    //Test case for Zero
     [Test]
     public void AnyOtherNumberTest2()
+    {
+        var solution = new Solution();
+        var expectedResult = solution.PrintFizzBuzz(28);
+        Assert.AreEqual("28", expectedResult);
+    }
+    
+    //Test case for Zero
+    [Test]
+    public void ZeroTest()
     {
         var solution = new Solution();
         var expectedResult = solution.PrintFizzBuzz(0);
@@ -53,10 +138,11 @@ public class SolutionTests
     
     //Test case for Negative number
     [Test]
-    public void AnyOtherNumberTest()
+    public void NegativeNumberTest()
     {
         var solution = new Solution();
         var expectedResult = solution.PrintFizzBuzz(-1);
         Assert.AreEqual("Given number -1 is Negative.", expectedResult);
     }
+}
 }
